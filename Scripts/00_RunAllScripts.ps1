7b9ff0a2c51f9ab0d1e8cafa52dc1dc508a8d4ce
George Walkey <gwalkey@users.noreply.github.com>
2016-02-03 12:02:34 -0500

    Switched to using .net Connection

--- Scripts/00_RunAllScripts.ps1
+++ Scripts/00_RunAllScripts.ps1
@@ -15,27 +15,23 @@
     ServerName, [SQLUser], [SQLPassword]
 
 .Outputs
-
+	
 	
 .NOTES
-	Testing TFS Versioning
+	
 	
 .LINK
-	
+	http://github.com/gwalkey
+
 #>
 
 
 Param(
   [string]$SQLInstance="localhost",
   [string]$myuser,
-  [string]$mypass,
-  [int]$timestamp = 0
+  [string]$mypass
 )
 
-# --- TIPS ---
-# Want to Register these or your own scripts as a Powershell Module?
-# Rename them from .ps1 to .psm1 and put them in one of the folders pointed to by
-# $env:PSModulePath (the Windows Environment path)
 
 cls
 
@@ -45,63 +41,85 @@ cls
 Import-Module ".\LoadSQLSmo.psm1"
 LoadSQLSMO
 
-# Load More Assemblies
-# Add-type �assemblyname system.speech | Out-Null
-# $talker = New-Object System.Speech.Synthesis.SpeechSynthesizer
-
 $startTime = get-date
 
 #  Script Name
 Write-Host -f Yellow -b Black "00 - RunAllScripts"
-#$talker.speak("Scripting started at $((Get-Date).ToShortTimeString())")
-
-# assume localhost
-if ($SQLInstance.length -eq 0)
-{
-	Write-Output "Assuming localhost"
-	$Sqlinstance = 'localhost'
-}
-
 
 # Server connection check
-if ($mypass.Length -ge 1 -and $myuser.Length -ge 1) 
+try
 {
-	Write-Output "$SQLInstance - Testing SQL Auth"
-	try
+    $old_ErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'SilentlyContinue'
+
+    if ($mypass.Length -ge 1 -and $myuser.Length -ge 1) 
     {
-        $results = Invoke-SqlCmd -ServerInstance $SQLInstance -Query "select serverproperty('productversion')" -Username $myuser -Password $mypass -QueryTimeout 10 #-erroraction SilentlyContinue
-        if($results -ne $null)
-        {
-            $myver = $results.Column1
-            Write-Output $myver
-        }	
-	}
-	catch
-    {
-		Write-Host -f red "$SQLInstance not installed/running or is offline - Try Windows Auth?"
-		exit
-	}
-}
-else
-{
-	Write-Output "$SQLInstance - Testing Windows Auth"
- 	Try
-    {
-        $results = Invoke-SqlCmd -ServerInstance $SQLInstance -Query "select serverproperty('productversion')" -QueryTimeout 10 -erroraction SilentlyContinue
-        if($results -ne $null)
-        {
-            $myver = $results.Column1
-            Write-Output $myver
-        }
+        Write-Output "Testing SQL Auth"
+		# .NET Method
+		# Open connection and Execute sql against server
+		$DataSet = New-Object System.Data.DataSet
+		$SQLConnectionString = "Data Source=$SQLInstance;User ID=$myuser;Password=$mypass;"
+		$Connection = New-Object System.Data.SqlClient.SqlConnection
+		$Connection.ConnectionString = $SQLConnectionString
+		$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+		$SqlCmd.CommandText = "select serverproperty('productversion')"
+		$SqlCmd.Connection = $Connection
+		$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+		$SqlAdapter.SelectCommand = $SqlCmd
+    
+		# Insert results into Dataset table
+		$SqlAdapter.Fill($DataSet) | out-null
+
+		# Close connection to sql server
+		$Connection.Close()
+		$results = $DataSet.Tables[0].Rows[0]
+
+		# SQLCMD.EXE Method
+        #$results = Invoke-SqlCmd -ServerInstance $SQLInstance -Query "select serverproperty('productversion')" -Username $myuser -Password $mypass -QueryTimeout 10 -erroraction SilentlyContinue
+        $serverauth="sql"
     }
-	catch
+    else
     {
-	    Write-Host -f red "$SQLInstance not installed/running or is offline - Try SQL Auth?" 
-	    exit
-	}
+        Write-Output "Testing Windows Auth"
+		# .NET Method
+		# Open connection and Execute sql against server using Windows Auth
+		$DataSet = New-Object System.Data.DataSet
+		$SQLConnectionString = "Data Source=$SQLInstance;Integrated Security=SSPI;"
+		$Connection = New-Object System.Data.SqlClient.SqlConnection
+		$Connection.ConnectionString = $SQLConnectionString
+		$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+		$SqlCmd.CommandText = "select serverproperty('productversion')"
+		$SqlCmd.Connection = $Connection
+		$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+		$SqlAdapter.SelectCommand = $SqlCmd
+    
+		# Insert results into Dataset table
+		$SqlAdapter.Fill($DataSet) | out-null
+
+		# Close connection to sql server
+		$Connection.Close()
+		$results = $DataSet.Tables[0].Rows[0]
+
+		# SQLCMD.EXE Method
+    	#$results = Invoke-SqlCmd -ServerInstance $SQLInstance -Query "select serverproperty('productversion')" -QueryTimeout 10 -erroraction SilentlyContinue
+        $serverauth = "win"
+    }
+
+    if($results -ne $null)
+    {
+        Write-Output ("SQL Version: {0}" -f $results.Column1)
+    }
+
+    # Reset default PS error handler
+    $ErrorActionPreference = $old_ErrorActionPreference 	
 
 }
-
+catch
+{
+    Write-Host -f red "$SQLInstance appears offline - Try Windows Authorization."
+    Set-Location $BaseFolder
+	exit
+}
 
 set-location $BaseFolder
 
@@ -136,29 +154,17 @@ set-location $BaseFolder
 & .\17_Managed_Backups.ps1 $SQLInstance $myuser $mypass
 # & .\18_Replication.ps1 $SQLInstance $myuser $mypass
 # & .\19_AlwaysOn.ps1 $SQLInstance $myuser $mypass
+& .\20_DataBase_Objects.ps1 $SQLInstance $myuser $mypass
 & .\21_Dac_Packages.ps1 $SQLInstance $myuser $mypass
 & .\22_Policy_Based_Mgmt.ps1 $SQLInstance $myuser $mypass
 & .\23_Database_Diagrams.ps1 $SQLInstance $myuser $mypass
 & .\24_Plan_Guides.ps1 $SQLInstance $myuser $mypass
 
 
-
-
 Write-Output "`r`n"
 $ElapsedTime = ((get-date) - $startTime)
 Write-Output ("$SQLInstance Elapsed time: {0:00}:{1:00}:{2:00}.{3:0000}" -f $ElapsedTime.Hours,$ElapsedTime.Minutes,$ElapsedTime.Seconds, $ElapsedTime.TotalMilliseconds)
 
-# TimeStamp the output folder for DIFF Operations
-if($timestamp -eq 1)
-{
-    $currentTime = get-date -f yyyy_MM_dd_HH_mm_ss
-    $oldFoldername = $baseFolder+"\"+$sqlinstance
-    $NewFolderName = $baseFolder+"\"+$sqlinstance+"_"+$currentTime
-    rename-item -path $oldFoldername -NewName $NewFolderName -force
-}
-
-# $talker = New-Object System.Speech.Synthesis.SpeechSynthesizer
-# $talker.speak("Scripting Success. You are the Man $env:USERNAME")
 
 set-location $BaseFolder
 exit
