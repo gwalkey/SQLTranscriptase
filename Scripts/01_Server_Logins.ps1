e29550befeff943269692de635a8d4bd1eb1ad22
George Walkey <george@insyncva.com>
2016-02-28 19:42:35 -0500

    01_Logins - Added Hashed Passwords and SIDs
    Hashed

--- Scripts/01_Server_Logins.ps1
+++ Scripts/01_Server_Logins.ps1
@@ -466,6 +466,291 @@ foreach ($Login in $Logins)
 
 }
  
+
+# MSDN Login Transfer Script (Passwords, SIDs)
+# https://support.microsoft.com/en-us/kb/918992
+
+# Creare Hex SP First
+$mySQL5 = 
+"
+USE [master]
+
+IF (OBJECT_ID('sp_hexadecimal') IS NOT NULL)
+  DROP PROCEDURE [sp_hexadecimal]
+
+exec('
+CREATE PROCEDURE [dbo].[sp_hexadecimal]
+    @binvalue varbinary(256),
+    @hexvalue varchar (514) OUTPUT
+AS
+DECLARE @charvalue varchar (514)
+DECLARE @i int
+DECLARE @length int
+DECLARE @hexstring char(16)
+SELECT @charvalue = ''0x''
+SELECT @i = 1
+SELECT @length = DATALENGTH (@binvalue)
+SELECT @hexstring = ''0123456789ABCDEF''
+WHILE (@i <= @length)
+BEGIN
+  DECLARE @tempint int
+  DECLARE @firstint int
+  DECLARE @secondint int
+  SELECT @tempint = CONVERT(int, SUBSTRING(@binvalue,@i,1))
+  SELECT @firstint = FLOOR(@tempint/16)
+  SELECT @secondint = @tempint - (@firstint*16)
+  SELECT @charvalue = @charvalue +
+    SUBSTRING(@hexstring, @firstint+1, 1) +
+    SUBSTRING(@hexstring, @secondint+1, 1)
+  SELECT @i = @i + 1
+END
+
+SET @hexvalue = @charvalue;
+')
+
+
+
+"
+
+# connect correctly
+if ($serverauth -eq "win")
+{
+	# .NET Method
+	# Open connection and Execute sql against server using Windows Auth
+	$DataSet = New-Object System.Data.DataSet
+	$SQLConnectionString = "Data Source=$SQLInstance;Integrated Security=SSPI;"
+	$Connection = New-Object System.Data.SqlClient.SqlConnection
+	$Connection.ConnectionString = $SQLConnectionString
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $mySQL5
+	$SqlCmd.Connection = $Connection
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+    
+	# Insert results into Dataset table
+	$SqlAdapter.Fill($DataSet) | out-null
+
+    # Eval Return Set
+    if ($DataSet.Tables.Count -ne 0) 
+    {
+	    $sqlresults5 = $DataSet.Tables[0]
+    }
+    else
+    {
+        $sqlresults5 =$null
+    }
+
+    # Close connection to sql server
+	$Connection.Close()
+
+}
+else
+{
+	# .NET Method
+	# Open connection and Execute sql against server
+	$DataSet = New-Object System.Data.DataSet
+	$SQLConnectionString = "Data Source=$SQLInstance;User ID=$myuser;Password=$mypass;"
+	$Connection = New-Object System.Data.SqlClient.SqlConnection
+	$Connection.ConnectionString = $SQLConnectionString
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $mySQLquery5
+	$SqlCmd.Connection = $Connection
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+    
+	# Insert results into Dataset table
+	$SqlAdapter.Fill($DataSet) | out-null
+
+    # Eval Return Set
+    if ($DataSet.Tables.Count -gt 0) 
+    {
+	    $sqlresults5 = $DataSet.Tables[0]
+    }
+    else
+    {
+        $sqlresults5 =$null
+    }
+
+    # Close connection to sql server
+	$Connection.Close()
+
+}
+
+
+
+# Get Logins with Passwords and SIDs
+$mySQL6 = 
+"
+set nocount on
+
+DECLARE @OutputTable TABLE
+(
+	outstring nvarchar(max)
+)
+
+DECLARE @name sysname
+DECLARE @type varchar (1)
+DECLARE @hasaccess int
+DECLARE @denylogin int
+DECLARE @is_disabled int
+DECLARE @PWD_varbinary  varbinary (256)
+DECLARE @PWD_string  varchar (514)
+DECLARE @SID_varbinary varbinary (85)
+DECLARE @SID_string varchar (514)
+DECLARE @tmpstr  varchar (1024)
+DECLARE @is_policy_checked varchar (3)
+DECLARE @is_expiration_checked varchar (3)
+DECLARE @defaultdb sysname
+ 
+DECLARE login_curs CURSOR FOR
+SELECT p.sid, p.name, p.type, p.is_disabled, p.default_database_name, l.hasaccess, l.denylogin 
+FROM sys.server_principals p 
+LEFT JOIN sys.syslogins l
+ON ( l.name = p.name ) WHERE p.type IN ( 'S', 'G', 'U' ) AND p.name <> 'sa'
+
+OPEN login_curs
+
+FETCH NEXT FROM login_curs INTO @SID_varbinary, @name, @type, @is_disabled, @defaultdb, @hasaccess, @denylogin
+
+WHILE (@@fetch_status <> -1)
+BEGIN
+  IF (@@fetch_status <> -2)
+  BEGIN    
+    IF (@type IN ( 'G', 'U'))
+    BEGIN -- NT authenticated account/group
+      SET @tmpstr = 'CREATE LOGIN ' + QUOTENAME( @name ) + ' FROM WINDOWS WITH DEFAULT_DATABASE = [' + @defaultdb + ']'
+    END
+    ELSE BEGIN -- SQL Server authentication
+        -- obtain password and sid
+        SET @PWD_varbinary = CAST( LOGINPROPERTY( @name, 'PasswordHash' ) AS varbinary (256) )
+        EXEC sp_hexadecimal @PWD_varbinary, @PWD_string OUT
+        EXEC sp_hexadecimal @SID_varbinary,@SID_string OUT
+ 
+        -- obtain password policy state
+        SELECT @is_policy_checked = CASE is_policy_checked WHEN 1 THEN 'ON' WHEN 0 THEN 'OFF' ELSE NULL END FROM sys.sql_logins WHERE name = @name
+        SELECT @is_expiration_checked = CASE is_expiration_checked WHEN 1 THEN 'ON' WHEN 0 THEN 'OFF' ELSE NULL END FROM sys.sql_logins WHERE name = @name
+ 
+        SET @tmpstr = 'CREATE LOGIN ' + QUOTENAME( @name ) + ' WITH PASSWORD = ' + @PWD_string + ' HASHED, SID = ' + @SID_string + ', DEFAULT_DATABASE = [' + @defaultdb + ']'
+
+        IF ( @is_policy_checked IS NOT NULL )
+        BEGIN
+          SET @tmpstr = @tmpstr + ', CHECK_POLICY = ' + @is_policy_checked
+        END
+        IF ( @is_expiration_checked IS NOT NULL )
+        BEGIN
+          SET @tmpstr = @tmpstr + ', CHECK_EXPIRATION = ' + @is_expiration_checked
+        END
+    END
+    IF (@denylogin = 1)
+    BEGIN -- login is denied access
+      SET @tmpstr = @tmpstr + '; DENY CONNECT SQL TO ' + QUOTENAME( @name )
+    END
+    ELSE IF (@hasaccess = 0)
+    BEGIN -- login exists but does not have access
+      SET @tmpstr = @tmpstr + '; REVOKE CONNECT SQL TO ' + QUOTENAME( @name )
+    END
+    IF (@is_disabled = 1)
+    BEGIN -- login is disabled
+      SET @tmpstr = @tmpstr + '; ALTER LOGIN ' + QUOTENAME( @name ) + ' DISABLE'
+    END
+	-- Add To output table
+    insert into @OutputTable (outstring) values (@tmpstr)
+  END
+
+  FETCH NEXT FROM login_curs INTO @SID_varbinary, @name, @type, @is_disabled, @defaultdb, @hasaccess, @denylogin
+   END
+CLOSE login_curs
+deallocate login_curs
+
+select * from @OutputTable
+
+"
+
+# connect correctly
+if ($serverauth -eq "win")
+{
+	# .NET Method
+	# Open connection and Execute sql against server using Windows Auth
+	$DataSet = New-Object System.Data.DataSet
+	$SQLConnectionString = "Data Source=$SQLInstance;Integrated Security=SSPI;"
+	$Connection = New-Object System.Data.SqlClient.SqlConnection
+	$Connection.ConnectionString = $SQLConnectionString
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $mySQL6
+	$SqlCmd.Connection = $Connection
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+    
+	# Insert results into Dataset table
+	$SqlAdapter.Fill($DataSet) | out-null
+
+    # Eval Return Set
+    if ($DataSet.Tables[0].Rows.Count -ne 0) 
+    {
+	    $sqlresults6 = $DataSet.Tables[0].Rows
+    }
+    else
+    {
+        $sqlresults6 =$null
+    }
+
+    # Close connection to sql server
+	$Connection.Close()
+
+}
+else
+{
+	# .NET Method
+	# Open connection and Execute sql against server
+	$DataSet = New-Object System.Data.DataSet
+	$SQLConnectionString = "Data Source=$SQLInstance;User ID=$myuser;Password=$mypass;"
+	$Connection = New-Object System.Data.SqlClient.SqlConnection
+	$Connection.ConnectionString = $SQLConnectionString
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $mySQL6
+	$SqlCmd.Connection = $Connection
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+    
+	# Insert results into Dataset table
+	$SqlAdapter.Fill($DataSet) | out-null
+
+    # Eval Return Set
+    if ($DataSet.Tables.Count -gt 0) 
+    {
+	    $sqlresults6 = $DataSet.Tables[0].Rows
+    }
+    else
+    {
+        $sqlresults6 =$null
+    }
+
+    # Close connection to sql server
+	$Connection.Close()
+
+}
+
+if ($sqlresults6 -ne $null)
+{
+    Write-Output ("SQL Logins with Passwords and SIDs: {0}" -f $sqlresults6.count)
+    $myoutputfile4 = $output_path+"\06_MSDN_Logins.sql"
+    if(test-path -path $myoutputfile4)
+    {
+         remove-item -path $myoutputfile4 -force | Out-Null
+         Add-Content -Value "Logins for $sqlinstance `r`n" -Path $myoutputfile4 -Force
+    }
+
+    foreach ($myLogin in $sqlresults6)
+    {
+        $myLogin.outstring | Out-File -FilePath $myoutputfile4 -Encoding ascii -append -width 1000
+    }
+}
+else
+{
+    Write-Output "No Logins Found"
+}
+
+
 # Return To Base
 set-location $BaseFolder
 
