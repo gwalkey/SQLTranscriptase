f50290f5a7aa3ba539c51be59e138be42adabf65
George Walkey <gwalkey@users.noreply.github.com>
2016-03-29 08:31:27 -0400

    Replaced invoke-sqlcmd with .net data adapter

--- Scripts/04_Agent_Schedules.ps1
+++ Scripts/04_Agent_Schedules.ps1
@@ -54,26 +54,7 @@ if ($SQLInstance.Length -eq 0)
 Write-Output "Server $SQLInstance"
 
 
-$sql = 
-"
-
-SELECT     
-	'Exec msdb.dbo.sp_add_schedule '+
-	' @schedule_name='+char(39)+[name]+char(39)+
-	' ,@enabled=' + CASE [enabled] WHEN 1 THEN '1' WHEN 0 THEN '0' END+
-	' ,@freq_type=' + convert(varchar(4),[freq_type])+
-    ' ,@freq_interval=' +convert(varchar(3),[freq_interval])+
-	' ,@freq_subday_type='+convert(varchar(3),[freq_subday_type])+
-	' ,@freq_subday_interval='+convert(varchar(3),[freq_subday_interval])+
-	' ,@freq_relative_interval='+convert(varchar(3),[freq_relative_interval])+
-    ' ,@freq_recurrence_Factor='+convert(varchar(3),[freq_recurrence_factor])+
-	' ,@active_start_date='+convert(varchar(8),[active_start_date])+
-	' ,@active_end_date='+convert(varchar(8),[active_end_date])+
-	' ,@active_start_time='+convert(varchar(8),[active_start_time])+
-	' ,@active_end_time='+convert(varchar(8),[active_end_time])
-FROM [msdb].[dbo].[sysschedules]
-"
-
+# Create Output Folder
 $fullfolderPath = "$BaseFolder\$SQLInstance\04 - Agent Schedules"
 if(!(test-path -path $fullfolderPath))
 {
@@ -160,14 +141,70 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 $EditionSQL = "SELECT SERVERPROPERTY('Edition')"
 
+# connect correctly
 if ($serverauth -eq "win")
 {
-    $Edition = Invoke-SqlCmd -query $EditionSQL -Server $SQLInstance
+	# .NET Method
+	# Open connection and Execute sql against server using Windows Auth
+	$DataSet = New-Object System.Data.DataSet
+	$SQLConnectionString = "Data Source=$SQLInstance;Integrated Security=SSPI;"
+	$Connection = New-Object System.Data.SqlClient.SqlConnection
+	$Connection.ConnectionString = $SQLConnectionString
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $EditionSQL
+	$SqlCmd.Connection = $Connection
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+    
+	# Insert results into Dataset table
+	$SqlAdapter.Fill($DataSet) | out-null
+
+    # Eval Return Set
+    if ($DataSet.Tables.Count -gt 0) 
+    {
+	    $Edition = $DataSet.Tables[0]
+    }
+    else
+    {
+        $Edition =$null
+    }
+
+    # Close connection to sql server
+	$Connection.Close()
+
 }
 else
 {
-    $Edition = Invoke-SqlCmd -query $EditionSQL  -Server $SQLInstance –Username $myuser –Password $mypass    
+	# .NET Method
+	# Open connection and Execute sql against server
+	$DataSet = New-Object System.Data.DataSet
+	$SQLConnectionString = "Data Source=$SQLInstance;User ID=$myuser;Password=$mypass;"
+	$Connection = New-Object System.Data.SqlClient.SqlConnection
+	$Connection.ConnectionString = $SQLConnectionString
+	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	$SqlCmd.CommandText = $EditionSQL
+	$SqlCmd.Connection = $Connection
+	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	$SqlAdapter.SelectCommand = $SqlCmd
+    
+	# Insert results into Dataset table
+	$SqlAdapter.Fill($DataSet) | out-null
+
+    # Eval Return Set
+    if ($DataSet.Tables.Count -gt 0) 
+    {
+	    $Edition = $DataSet.Tables[0]
+    }
+    else
+    {
+        $Edition =$null
+    }
+
+    # Close connection to sql server
+	$Connection.Close()
+
 }
+
 
 if ($Edition -ne $null )
 {
@@ -178,8 +215,32 @@ if ($Edition -ne $null )
     }    
 }
 
+
 # Reset default PS error handler
 $ErrorActionPreference = $old_ErrorActionPreference 
+
+
+# Get Agent Schedules
+$sql = 
+"
+
+SELECT     
+	'Exec msdb.dbo.sp_add_schedule '+
+	' @schedule_name='+char(39)+[name]+char(39)+
+	' ,@enabled=' + CASE [enabled] WHEN 1 THEN '1' WHEN 0 THEN '0' END+
+	' ,@freq_type=' + convert(varchar(4),[freq_type])+
+    ' ,@freq_interval=' +convert(varchar(3),[freq_interval])+
+	' ,@freq_subday_type='+convert(varchar(3),[freq_subday_type])+
+	' ,@freq_subday_interval='+convert(varchar(3),[freq_subday_interval])+
+	' ,@freq_relative_interval='+convert(varchar(3),[freq_relative_interval])+
+    ' ,@freq_recurrence_Factor='+convert(varchar(3),[freq_recurrence_factor])+
+	' ,@active_start_date='+convert(varchar(8),[active_start_date])+
+	' ,@active_end_date='+convert(varchar(8),[active_end_date])+
+	' ,@active_start_time='+convert(varchar(8),[active_start_time])+
+	' ,@active_end_time='+convert(varchar(8),[active_end_time])
+FROM [msdb].[dbo].[sysschedules]
+"
+
 
 # Turn off default error handler
 $old_ErrorActionPreference = $ErrorActionPreference
