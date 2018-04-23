6439f0eab301e295099004c7cebe2b1a34dd0475
George Walkey <gwalkey@users.noreply.github.com>
2018-04-23 14:01:12 -0400

    Add files via upload

--- Scripts/01_Server_Resource_Governor.ps1
+++ Scripts/01_Server_Resource_Governor.ps1
@@ -26,107 +26,23 @@
 	
 #>
 
+[CmdletBinding()]
 Param(
   [string]$SQLInstance='localhost',
   [string]$myuser,
   [string]$mypass
 )
 
-Set-StrictMode -Version latest;
-
-[string]$BaseFolder = (Get-Item -Path ".\" -Verbose).FullName
-
-#  Script Name
-Write-Host  -f Yellow -b Black "01 - Server Resource Governor"
-
-
-# Usage Check
-if ($SQLInstance.Length -eq 0) 
-{
-    Write-host -f yellow "Usage: ./01_Server_Resource_Governor.ps1 `"SQLServerName`" ([`"Username`"] [`"Password`"] if DMZ machine)"
-    Set-Location $BaseFolder
-    exit
-}
-
-# Working
-Write-Output "Server $SQLInstance"
-
-# Load SMO Assemblies
+# Load Common Modules and .NET Assemblies
+Import-Module ".\SQLTranscriptase.psm1"
 Import-Module ".\LoadSQLSmo.psm1"
 LoadSQLSMO
 
-
-
-# Server connection check
-try
-{
-    $old_ErrorActionPreference = $ErrorActionPreference
-    $ErrorActionPreference = 'SilentlyContinue'
-
-    if ($mypass.Length -ge 1 -and $myuser.Length -ge 1) 
-    {
-        Write-Output "Testing SQL Auth"
-		# .NET Method
-		# Open connection and Execute sql against server
-		$DataSet = New-Object System.Data.DataSet
-		$SQLConnectionString = "Data Source=$SQLInstance;User ID=$myuser;Password=$mypass;"
-		$Connection = New-Object System.Data.SqlClient.SqlConnection
-		$Connection.ConnectionString = $SQLConnectionString
-		$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-		$SqlCmd.CommandText = "select serverproperty('productversion')"
-		$SqlCmd.Connection = $Connection
-		$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-		$SqlAdapter.SelectCommand = $SqlCmd
-    
-		# Insert results into Dataset table
-		$SqlAdapter.Fill($DataSet) | out-null
-
-		# Close connection to sql server
-		$Connection.Close()
-		$results = $DataSet.Tables[0].Rows[0]
-
-        $serverauth="sql"
-    }
-    else
-    {
-        Write-Output "Testing Windows Auth"
-		# .NET Method
-		# Open connection and Execute sql against server using Windows Auth
-		$DataSet = New-Object System.Data.DataSet
-		$SQLConnectionString = "Data Source=$SQLInstance;Integrated Security=SSPI;"
-		$Connection = New-Object System.Data.SqlClient.SqlConnection
-		$Connection.ConnectionString = $SQLConnectionString
-		$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-		$SqlCmd.CommandText = "select serverproperty('productversion')"
-		$SqlCmd.Connection = $Connection
-		$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-		$SqlAdapter.SelectCommand = $SqlCmd
-    
-		# Insert results into Dataset table
-		$SqlAdapter.Fill($DataSet) | out-null
-
-		# Close connection to sql server
-		$Connection.Close()
-		$results = $DataSet.Tables[0].Rows[0]
-
-        $serverauth = "win"
-    }
-
-    if($results -ne $null)
-    {
-        Write-Output ("SQL Version: {0}" -f $results.Column1)
-    }
-
-    # Reset default PS error handler
-    $ErrorActionPreference = $old_ErrorActionPreference 	
-
-}
-catch
-{
-    Write-Host -f red "$SQLInstance appears offline - Try Windows Authorization."
-    Set-Location $BaseFolder
-	exit
-}
+# Init
+Set-StrictMode -Version latest;
+[string]$BaseFolder = (Get-Item -Path ".\" -Verbose).FullName
+Write-Host  -f Yellow -b Black "01 - Server Resource Governor"
+Write-Output "Server $SQLInstance"
 
 
 function CopyObjectsToFiles($objects, $outDir) {
@@ -153,18 +69,46 @@ function CopyObjectsToFiles($objects, $outDir) {
 }
 
 
+# Server connection check
+$SQLCMD1 = "select serverproperty('productversion') as 'Version'"
+try
+{
+    if ($mypass.Length -ge 1 -and $myuser.Length -ge 1) 
+    {
+        Write-Output "Testing SQL Auth"        
+        $myver = ConnectSQLAuth -SQLInstance $SQLInstance -Database "master" -SQLExec $SQLCMD1 -User $myuser -Password $mypass -ErrorAction Stop| select -ExpandProperty Version
+        $serverauth="sql"
+    }
+    else
+    {
+        Write-Output "Testing Windows Auth"
+		$myver = ConnectWinAuth -SQLInstance $SQLInstance -Database "master" -SQLExec $SQLCMD1 -ErrorAction Stop | select -ExpandProperty Version
+        $serverauth = "win"
+    }
 
-# Set Local Vars
-$server	= $SQLInstance
+    if($myver -ne $null)
+    {
+        Write-Output ("SQL Version: {0}" -f $myver)
+    }
 
+}
+catch
+{
+    Write-Host -f red "$SQLInstance appears offline."
+    Set-Location $BaseFolder
+	exit
+}
+
+
+# New Up SMO Object
 if ($serverauth -eq "win")
 {
-    $srv    = New-Object "Microsoft.SqlServer.Management.SMO.Server" $server
-    $scripter 	= New-Object ("Microsoft.SqlServer.Management.SMO.Scripter") ($server)
+    $srv    = New-Object "Microsoft.SqlServer.Management.SMO.Server" $SQLInstance
+    $scripter 	= New-Object ("Microsoft.SqlServer.Management.SMO.Scripter") ($SQLInstance)
 }
 else
 {
-    $srv = New-Object "Microsoft.SqlServer.Management.SMO.Server" $server
+    $srv = New-Object "Microsoft.SqlServer.Management.SMO.Server" $SQLInstance
     $srv.ConnectionContext.LoginSecure=$false
     $srv.ConnectionContext.set_Login($myuser)
     $srv.ConnectionContext.set_Password($mypass)    
@@ -209,40 +153,38 @@ $scripter.Options.SchemaQualifyForeignKeysReferences = $true
 $scripter.Options.ToFileOnly 			= $true
 $scripter.Options.XmlIndexes            = $true
 
-# Output Folder
+# Create Output Folder
 Set-Location $BaseFolder
 $output_path = "$BaseFolder\$SQLInstance\01 - Server Resource Governor\"
 if(!(test-path -path $output_path))
-    {
-        mkdir $output_path | Out-Null
-    }
+{
+    mkdir $output_path | Out-Null
+}
 
 try
 {
-    # Pools
+    # Get Pools
     $pools = $srv.ResourceGovernor.ResourcePools | where-object -FilterScript {$_.Name -notin "internal","default"}
     if ($pools.Count -gt 0)
     {
-        #CopyObjectsToFiles $pools $output_path
         $pools.script() | out-file -FilePath "$output_path\pools.sql" -append -encoding ascii
     }
 
-    # Workgroups
+    # Get Workgroups
     foreach ($pool in $pools)
     {
         
-        #Put Workgroups in parent pool's folder
+        # Put Workgroups in parent pool's folder
         $pool_path = "$BaseFolder\$SQLInstance\01 - Server Resource Governor\"+$pool.Name+"\"
         if(!(test-path -path $pool_path))
-            {
-                mkdir $pool_path | Out-Null
-            }
+        {
+            mkdir $pool_path | Out-Null
+        }
         
-        #Workgroup
+        # Workgroup
         $workloadgroups = $pool.WorkloadGroups
         foreach ($workloadgroup in $workloadgroups)
         {
-            #CopyObjectsToFiles $workloadgroup $pool_path
             $myWLgroupFile = $pool_path+"\"+$workloadgroup.name+".sql"
             $workloadgroup.script() | out-file -FilePath  $myWLgroupFile -append -encoding ascii
         }
@@ -252,7 +194,7 @@ try
 catch
 {
     $fullfolderpath = "$BaseFolder\$SQLInstance\"
-    echo null > "$fullfolderpath\01 - Server Resource Governor - not setup or enabled.txt"
+    "Server Resource Governor is not setup or enabled" > "$fullfolderpath\01 - Server Resource Governor is not setup or enabled.txt"
 }
 
 # Return To Base
